1581cdcd1ceab927569445ab527f42d1e6418475
Anton Bryantsev <chieffly@ya.ru>
2023-05-29 11:11:23 +0300

    data layer

---
+++ app/src/main/java/ru/chieffly/mvvmexercise/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package ru.chieffly.mvvmexercise.data
+
+import ru.chieffly.mvvmexercise.domain.model.ShopItem
+import ru.chieffly.mvvmexercise.domain.repository.ShopListRepository
+
+/**
+ *Created by Bryantsev Anton on 29.05.2023.
+ **/
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID)
+            shopItem.id = autoIncrementId++
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldItem = getShopItem(shopItem.id)
+        deleteShopItem(oldItem)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException(" Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
