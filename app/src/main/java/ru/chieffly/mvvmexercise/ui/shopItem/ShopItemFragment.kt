e0f3c574da5431a6b2ada9cfeef3486fc45b473c
Anton Bryantsev <anton.bryantsev@auxo.team>
2023-05-31 19:52:27 +0300

    improve ShopItemFragment

--- app/src/main/java/ru/chieffly/mvvmexercise/ui/shopItem/ShopItemFragment.kt
+++ app/src/main/java/ru/chieffly/mvvmexercise/ui/shopItem/ShopItemFragment.kt
@@ -20,10 +20,7 @@ import ru.chieffly.mvvmexercise.domain.model.ShopItem
  *Created by Bryantsev Anton on 30.05.2023.
  **/
 
-class ShopItemFragment(
-    private val screenMode: String = MODE_UNKNOWN,
-    private val shopItemId: Int = ShopItem.UNDEFINED_ID
-) : Fragment() {
+class ShopItemFragment: Fragment() {
 
     private lateinit var viewModel: ShopItemViewModel
 
@@ -33,6 +30,13 @@ class ShopItemFragment(
     private lateinit var etCount: TextInputEditText
     private lateinit var btnSave: Button
 
+    private var screenMode: String = MODE_UNKNOWN
+    private var shopItemId: Int = ShopItem.UNDEFINED_ID
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseParams()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_shop_item, container, false)
@@ -40,7 +44,6 @@ class ShopItemFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        parseParams()
         viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
         initViews(view)
         addTextChangeListeners()
@@ -142,28 +145,45 @@ class ShopItemFragment(
     }
 
     private fun parseParams() {
-        if (screenMode != MODE_EDIT && screenMode != MODE_ADD) {
+        val args = requireArguments()
+        if (!args.containsKey(SCREEN_MODE)){
             throw RuntimeException("Param screen mode is absent")
         }
-        if (screenMode == MODE_EDIT) {
-            if (shopItemId == ShopItem.UNDEFINED_ID) {
-                throw RuntimeException("Param shop item id is absent")
-            }
+        val mode = args.getString(SCREEN_MODE)
+        if (mode!= MODE_EDIT && mode!= MODE_ADD) {
+            throw RuntimeException("Unknown screen mode $mode")
         }
+        screenMode = mode
+
+        if (screenMode == MODE_EDIT)
+            if (!args.containsKey(SHOP_ITEM_ID)) {
+            throw RuntimeException("Param shop item id is absent")
+            }
+        shopItemId = args.getInt(SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
     }
 
     companion object {
-        private const val EXTRA_SCREEN_MODE = "extra_mode"
-        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val SCREEN_MODE = "extra_mode"
+        private const val SHOP_ITEM_ID = "extra_shop_item_id"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
         private const val MODE_UNKNOWN = ""
 
         fun newInstanceAddItem(): ShopItemFragment {
-            return ShopItemFragment(MODE_ADD)
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putString(SCREEN_MODE, MODE_ADD)
+                }
+            }
         }
+
         fun newInstanceEditItem(shopItemId: Int): ShopItemFragment {
-            return ShopItemFragment(MODE_EDIT, shopItemId)
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putString(SCREEN_MODE, MODE_EDIT)
+                    putInt(SHOP_ITEM_ID,shopItemId)
+                }
+            }
         }
     }
 }
